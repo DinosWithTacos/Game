460140fce86d5a2e3a269cb688a23b77a940ae46
Zachary <esteppzachary@gmail.ccm>
2020-11-24 19:57:14 -0500

    Update main.py

--- main/main.py
+++ main/main.py
@@ -1,1 +1,18 @@
-print('main. wow. just making a file for a place holder. who knew')
+while True:
+    answer = input("Do you like the winter? ")
+
+    y_list = ['y', 'yes', 'sure', 'yea']
+    n_list = ['n', 'no', 'nope', 'nada']
+
+    answer = answer.lower()
+
+    if answer in y_list:
+        print("Yes fucking amazing fuck.")
+        break
+    elif answer in n_list:
+        print('fuck off')
+        break
+    else:
+        print("what did you say")
+
+
