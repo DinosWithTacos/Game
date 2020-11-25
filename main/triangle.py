f492e18190060888c6c74c90cb29d335fca1227a
Foxtrotts <74994722+Foxtrotts@users.noreply.github.com>
2020-11-25 12:39:10 -0500

    bigger update
    Version 2.0 is finally out
    
    After countless hours of creating and refining, we are proud to announce version 2.0
    
    Version 2.0 will completely change how the game is played and will undoubtly change the course of this game forever.

---
+++ main/triangle.py
@@ -0,0 +1,27 @@
+
+character_name = "Larry"
+Character_age = "12"
+is_male = True
+print("There once was a man named " + character_name + ",")
+print("whose legs were very hairy, ")
+print("He had a great fall, and started to ball, ")
+print("Bush did 9/11.")
+print("also im " + Character_age + " lol")
+
+print("---------------------")
+
+print("\"Baluga\" Whale")
+
+phrase = "My Name Jeff"
+print(phrase.replace("Jeff", "Heffe"))
+
+my_num = -5
+print(round(36.4))
+
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Hello " + name +"! You are " + age)
+
+
